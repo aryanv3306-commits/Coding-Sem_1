d2ef96fb3a8373d5baeeb5b6f8751dbc2ea98701
aryanv3306-commits <aryanv3306@gmail.com>
2025-11-11 23:36:30 +0530

    code of factorial of a given  number

---
+++ FactorialOfAGivenNumber.c
@@ -0,0 +1,15 @@
+// program to find the factorial of a given number 
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number : ");
+    scanf("%d",&n);
+    int product=1;
+    for (int i=1;i<=n;i++)
+    {
+        product=product*i;
+    }
+    printf("factorial of %d is %d",n,product);
+    return 0;
+}
